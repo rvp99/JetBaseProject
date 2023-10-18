58ad7b3fe033dbbc21af687b57c780a88526d576
Serkan Alsabeit <serkan.alsabeyit@gmail.com>
2023-10-18 16:29:47 +0300

    DataAccess eklendi

---
+++ DataAccess/Concrete/EntityFramework/NortwindContext.cs
@@ -0,0 +1,18 @@
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace DataAccess.Concrete.EntityFramework;
+
+public class NortwindContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            @"Server=localhost;Database=NorthWind;User Id=SA;Password=tassadar99;TrustServerCertificate=True");
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+}
